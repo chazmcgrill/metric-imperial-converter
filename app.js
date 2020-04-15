636c21fd71e2d0bc4502dc07f7e2c613a0d8207c
chazmcgrill <charlietaylorcoder@gmail.com>
2020-04-15 18:18:31 +0100

    Adds number validation

--- app.js
+++ app.js
@@ -4,9 +4,34 @@ const app = express();
 
 app.set('view engine', 'pug');
 
+const numberFormatRegex = /^\d+[.]?(\d+)?\/?(\d+[.]?(\d+)?)?$/
+
+function validateNum(num) {
+    switch(true) {
+        case (num === ''):
+            return 1;
+
+        case (!numberFormatRegex.test(num)):
+            return false;
+
+        case (num.includes('/')):
+            const [a, b] = num.split('/').map(Number);
+            return a / b;
+
+        default:
+            return Number(num); 
+    }
+}
+
 function validateInput(input) {
-    const [value, unit] = input.split(/([A-z]+)/).filter(Boolean);
-    return { value, unit }
+    const [num, unit] = input.split(/([A-z]+)/);
+
+    if (!unit) return 'invalid unit'; // TODO: Add function to check against valid unit types
+    
+    let initNum = validateNum(num)
+    if (!initNum) return 'invalid number';
+
+    return { initNum, initUnit: unit }
 }
 
 app.post('/convert', (req, res) => {

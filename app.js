9eefe3e243f7107a03230aa18bcdfe5bf2707c2f
chazmcgrill <charlietaylorcoder@gmail.com>
2020-04-16 10:28:51 +0100

    Folder restructure

--- app.js
+++ app.js
@@ -2,88 +2,13 @@ const express = require('express');
 
 const app = express();
 
+const utils = require('./utils');
+
 app.set('view engine', 'pug');
-
-const NUMBER_FORMAT_REGEX = /^\d+([.]\d+)?(\/\d+([.]\d+)?)?$/;
-
-const UNITS_DATA = {
-    mi: {
-        longName: 'miles',
-        returnUnit: 'km',
-        conversion: (val) => val * 1.60934,
-    },
-    km: {
-        longName: 'kilometers',
-        returnUnit: 'mi',
-        conversion: (val) => val / 1.60934,
-    },
-    lbs: {
-        longName: 'pounds',
-        returnUnit: 'kg',
-        conversion: (val) => val / 2.20462,
-    },
-    kg: {
-        longName: 'kilograms',
-        returnUnit: 'lbs',
-        conversion: (val) => val * 2.20462,
-    },
-    gal: {
-        longName: 'gallons',
-        returnUnit: 'L',
-        conversion: (val) => val / 4.54609,
-    },
-    L: {
-        longName: 'litres',
-        returnUnit: 'gal',
-        conversion: (val) => val * 4.54609,
-    },
-};
-
-const fractionStringToDecimal = (str) => {
-    const [a, b] = str.split('/').map(Number);
-    return a / b;
-} 
-
-const validateNumberString = (num) => {
-    switch(true) {
-        case (num === ''):
-            return 1;
-
-        case (!NUMBER_FORMAT_REGEX.test(num)):
-            return false;
-
-        case (num.includes('/')):
-            return fractionStringToDecimal(num);
-
-        default:
-            return Number(num); 
-    }
-}
-
-const roundedNum = num => num.toFixed(5);
-
-const getConversionObject = (input) => {
-    const [num, initUnit] = input.split(/([A-z]+)/);
-    const unitData = UNITS_DATA[initUnit];
-
-    if (!unitData) return 'invalid unit'; 
-    
-    const formattedNum = validateNumberString(num);
-
-    if (!formattedNum) return 'invalid number';
-
-    const { longName, returnUnit, conversion } = unitData;
-    const returnNum = roundedNum(conversion(formattedNum));
-    const returnLongName = UNITS_DATA[returnUnit].longName;
-    const initNum = roundedNum(formattedNum);
-    const string = `${initNum} ${longName} converts to ${returnNum} ${returnLongName}`;
-
-    return { initNum, initUnit, returnNum, returnUnit, string };
-}
 
 app.post('/convert', (req, res) => {
     const { input } = req.query;
-    res.send(getConversionObject(input));
+    res.send(utils.getConversionObject(input));
 });
 
 app.get('/', (req, res) => {
